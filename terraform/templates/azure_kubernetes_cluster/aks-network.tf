ccdf9710055a0401549bb9088b57eef20ba89624
Will Schultz <will.schultz@ibm.com>
2023-01-31 11:07:35 -0600

    attempt at a second cluster alongside first

--- terraform/templates/azure_kubernetes_cluster/aks-network.tf
+++ terraform/templates/azure_kubernetes_cluster/aks-network.tf
@@ -14,3 +14,21 @@ resource "azurerm_subnet_route_table_association" "rt_association" {
   subnet_id      = azurerm_subnet.aks.id
   route_table_id = azurerm_route_table.route_table.id
 }
+
+
+resource "azurerm_network_security_group" "aks2-nsg" {
+  name                = "${azurerm_virtual_network.this.name}-${azurerm_subnet.aks2.name}-nsg"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet2" {
+  subnet_id                 = azurerm_subnet.aks2.id
+  network_security_group_id = azurerm_network_security_group.aks2-nsg.id
+}
+
+# # Associate Route Table to aks2 Subnet
+resource "azurerm_subnet_route_table_association" "rt_association2" {
+  subnet_id      = azurerm_subnet.aks2.id
+  route_table_id = azurerm_route_table.route_table.id
+}
--- terraform/templates/azure_kubernetes_cluster/firewall.tf
+++ terraform/templates/azure_kubernetes_cluster/firewall.tf
@@ -101,3 +101,64 @@ resource "azurerm_firewall_policy_rule_collection_group" "aks" {
     }
   }
 }
+
+resource "azurerm_firewall_policy_rule_collection_group" "aks2" {
+  name               = "aks-rcg-2"
+  firewall_policy_id = azurerm_firewall_policy.aks.id
+  priority           = 210
+  application_rule_collection {
+    name     = "aks_app_rules"
+    priority = 215
+    action   = "Allow"
+    rule {
+      name = "aks_service"
+      protocols {
+        type = "Https"
+        port = 443
+      }
+      source_addresses      = ["10.0.32.0/20"]
+      destination_fqdn_tags = ["AzureKubnernetesService"]
+    }
+  }
+
+  network_rule_collection {
+    name     = "aks_network_rules"
+    priority = 211
+    action   = "Allow"
+    rule {
+      name                  = "https"
+      protocols             = ["TCP"]
+      source_addresses      = ["10.0.32.0/20"]
+      destination_addresses = ["*"]
+      destination_ports     = ["443"]
+    }
+    rule {
+      name                  = "dns"
+      protocols             = ["UDP"]
+      source_addresses      = ["10.0.32.0/20"]
+      destination_addresses = ["*"]
+      destination_ports     = ["53"]
+    }
+    rule {
+      name                  = "time"
+      protocols             = ["UDP"]
+      source_addresses      = ["10.0.32.0/20"]
+      destination_addresses = ["*"]
+      destination_ports     = ["123"]
+    }
+    rule {
+      name                  = "tunnel_udp"
+      protocols             = ["UDP"]
+      source_addresses      = ["10.0.32.0/20"]
+      destination_addresses = ["*"]
+      destination_ports     = ["1194"]
+    }
+    rule {
+      name                  = "tunnel_tcp"
+      protocols             = ["TCP"]
+      source_addresses      = ["10.0.32.0/20"]
+      destination_addresses = ["*"]
+      destination_ports     = ["9000"]
+    }
+  }
+}
